7148c73920c8e85c88f47cdf28a7130624cdaba7
Ryan Aposhian <ryan.aposhian@fireflyautomatix.com>
2022-01-12 21:42:29 -0700

    started on the intergration between the frontend and the backend

--- server/controllers/getUsers.js
+++ server/controllers/getUsers.js
@@ -2,6 +2,7 @@ const axios = require('axios');
 
 module.exports = {
   getUsers: (request, response) => {
+    // TODO change this to return 7000 random users
     axios.get('https://randomuser.me/api/')
       .then(res => {
         response.status(200).send(res.data)
--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [users, setUsers] = useState({});
+
+  // TODO figure out how to get this effect to only fire once. Might not be able to use useState() hook.
+  useEffect(() => {
+    getUsers()
+  })
+  
+  const getUsers = () => {
+    axios.get(`/api/get-users`)
+        .then(response => setUsers(response.data.results[0]))
+        .catch(error => console.log(error))
+  };
+
+
+ console.log(users)
+
   return (
     <div className="App">
-      Test
+      {/* TODO figure out how to display the data correctly */}
+      (users) ? <p>{users.name.first}</p> : <p>No users</p>
     </div>
   );
 }
