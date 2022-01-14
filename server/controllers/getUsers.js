92347619c21643d5b2ed0b17b663649ba2d0dba8
Ryan Aposhian <ryan.aposhian@fireflyautomatix.com>
2022-01-13 19:28:20 -0700

    finished up the backend calls

--- server/controllers/getUsers.js
+++ server/controllers/getUsers.js
@@ -2,10 +2,18 @@ const axios = require('axios');
 
 module.exports = {
   getUsers: (request, response) => {
-    // TODO change this to return 7000 random users
-    axios.get('https://randomuser.me/api/')
+    //array to put the results into for returning to the frontend
+    let usersArr = [];
+    axios.get('https://randomuser.me/api/?results=3500')
       .then(res => {
-        response.status(200).send(res.data)
+        usersArr = res.data.results;
+        
+        axios.get('https://randomuser.me/api/?results=3500')
+        .then(r => {
+          //adding the second set of people to the frontend to take the total to 7000
+            usersArr = [...usersArr, ...r.data.results];
+            response.status(200).send(usersArr);
+          })
       })
       .catch(error => response.status(500).send(error));
   }
